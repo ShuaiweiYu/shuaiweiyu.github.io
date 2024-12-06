6bd6bf5fd7ca3f90f930eb64d955fe6ed24c32eb
Shuaiwei Yu <shuaiwei.yu@tum.de>
2024-12-07 00:03:27 +0100

    add skills and hobbys view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
-import {Typography, Box, Container, Paper} from '@mui/material';
-import { useInView } from 'react-intersection-observer'; // 引入 Intersection Observer Hook
-import { motion } from 'framer-motion'; // 引入动画库
-import Grid from '@mui/material/Grid';
+import {Typography, Box, Container, Paper, Grid, Card, CardContent, Stack, Divider} from '@mui/material';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+
+const IconCard = ({ type, iconSrc, altText, text, description }) => (
+    <Card sx={{ minWidth: 100, maxWidth: 250 }}>
+        <CardContent>
+            {/* 用于图标和文本的内容容器 */}
+            <Stack
+                direction="row"
+                divider={<Divider orientation="vertical" flexItem />}
+                spacing={1}
+                alignItems="flex-start" // 确保内容垂直对齐到顶部
+            >
+                <img src={iconSrc} alt={altText} style={styles.icon} />
+                <Stack spacing={0}>
+                    <Typography
+                        sx={{ fontSize: 14, textAlign: 'left' }}
+                        color="text.secondary"
+                        gutterBottom
+                    >
+                        {type}
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ fontSize: 20, textAlign: 'left', fontWeight: 600 }}
+                    >
+                        {text}
+                    </Typography>
+                </Stack>
+            </Stack>
+
+            {/* 用于描述的文本 */}
+            {description && (
+                <Typography
+                    variant="body2"
+                    sx={{ textAlign: 'left', mt: 2 }}
+                >
+                    {description}
+                </Typography>
+            )}
+        </CardContent>
+    </Card>
+);
+
+
 
 const HomePage = () => {
     // 使用 Intersection Observer 来检测元素是否在视口中
@@ -57,14 +100,14 @@ const HomePage = () => {
                     initial={{opacity: 0, y: 50}}
                     animate={inView1 ? {opacity: 1, y: 0} : {}}
                     transition={{duration: 0.6}}
-                    style={{height: '100%'}} // 添加此行
+                    style={{height: '100%'}}
                 >
                     <Paper
                         elevation={3}
                         style={{
                             ...styles.paper,
                             textAlign: 'left', // 左对齐文本
-                            height: '100%', // 添加此行
+                            height: '100%',
                         }}
                     >
                         <Grid container spacing={2} alignItems="center" direction="row">
@@ -75,7 +118,7 @@ const HomePage = () => {
                                     alt="俞帅威"
                                     style={{
                                         width: '100%',
-                                        height: '300px', // 修改此行
+                                        height: '300px',
                                         objectFit: 'cover',
                                     }}
                                 />
@@ -108,10 +151,10 @@ const HomePage = () => {
                 >
                     <Paper elevation={3} style={styles.paper}>
                         <Typography variant="h4" gutterBottom>
-                            教育背景
+                            我最近在做什么
                         </Typography>
                         <Typography variant="body1" paragraph>
-                            本科阶段学习了计算机科学的基础课程，如计算机导论、软件工程、数据库、算法与数据结构以及计算机网络等。研究生阶段侧重于商业软件开发、云计算和深度学习等领域的学习。
+                            Text
                         </Typography>
                     </Paper>
                 </motion.div>
@@ -125,10 +168,10 @@ const HomePage = () => {
                 >
                     <Paper elevation={3} style={styles.paper}>
                         <Typography variant="h4" gutterBottom>
-                            实习经历
+                            我做过什么？
                         </Typography>
                         <Typography variant="body1" paragraph>
-                            曾在上海的乐高集团分部和德国慕尼黑的雷斯康公司担任全栈开发实习生，还在德国宝马集团担任项目管理实习生。这些经历提升了我的代码和业务能力，加深了对软件开发周期的理解。
+                            Text
                         </Typography>
                     </Paper>
                 </motion.div>
@@ -141,12 +184,176 @@ const HomePage = () => {
                     transition={{duration: 0.6, delay: 0.6}}
                 >
                     <Paper elevation={3} style={styles.paper}>
-                        <Typography variant="h4" gutterBottom>
-                            兴趣爱好
+                        <Typography
+                            variant="h4"
+                            gutterBottom
+                            sx={{ textAlign: 'center' }} // 确保居中
+                        >
+                            趣闻
                         </Typography>
-                        <Typography variant="body1" paragraph>
-                            热爱研究新技术，喜欢看书、看电影，热衷于登山和徒步，对生活充满热情。
+
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                            sx={{ textAlign: 'left' }} // 确保左对齐
+                        >
+                            编程技能
                         </Typography>
+                        <Stack 
+                            spacing={{ xs: 1, sm: 2 }}
+                            direction="row" 
+                            useFlexGap
+                            sx={{ flexWrap: 'wrap' }}
+                            alignItems="flex-start"
+                        >
+                            <IconCard
+                            type="Progamming Language"
+                                iconSrc="/assets/java.png"
+                                altText="Java"
+                                text="Java"
+                                description="I never been to Java Island"
+                            />
+                            <IconCard
+                                type="Progamming Language"
+                                iconSrc="/assets/python.png"
+                                altText="Python"
+                                text="Python"
+                            />
+                            <IconCard
+                                type="Progamming Language"
+                                iconSrc="/assets/javascript.png"
+                                altText="JavaScript"
+                                text="JavaScript"
+                                description="ChatGPT is taking over"
+                            />
+                            <IconCard
+                                type="Progamming Language"
+                                iconSrc="/assets/swift.png"
+                                altText="Swift"
+                                text="Swift"
+                                description=""
+                            />
+                            <IconCard
+                                type="Framework"
+                                iconSrc="/assets/springboot.webp"
+                                altText="SpringBoot"
+                                text="SpringBoot"
+                                description=""
+                            />
+                            <IconCard
+                                type="Framework"
+                                iconSrc="/assets/react.png"
+                                altText="React.js"
+                                text="React.js"
+                                description=""
+                            />
+                            <IconCard
+                                type="Framework"
+                                iconSrc="/assets/vue.png"
+                                altText="Vue.js"
+                                text="Vue.js"
+                                description=""
+                            />
+                            <IconCard
+                                type="Version Control"
+                                iconSrc="/assets/git.png"
+                                altText="Git"
+                                text="Git"
+                                description="I love to check git blame"
+                            />
+                            <IconCard
+                                type="Database"
+                                iconSrc="/assets/mongo.webp"
+                                altText="Mongo DB"
+                                text="Mongo DB"
+                                description=""
+                            />
+                            <IconCard
+                                type="Database"
+                                iconSrc="/assets/mysql.png"
+                                altText="MySQL"
+                                text="MySQL"
+                                description=""
+                            />
+                            <IconCard
+                                type="Middleware"
+                                iconSrc="/assets/kafka.webp"
+                                altText="Kafka"
+                                text="Kafka"
+                                description=""
+                            />
+                            <IconCard
+                                type="Cloud Service"
+                                iconSrc="/assets/aws.webp"
+                                altText="AWS"
+                                text="AWS"
+                                description=""
+                            />
+                        </Stack>
+
+                        <Divider sx={{ padding: '8px' }}/>
+
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                            sx={{ textAlign: 'left' }}
+                        >
+                            我会说什么语言？
+                        </Typography>
+                        <Stack 
+                            spacing={{ xs: 1, sm: 2 }}
+                            direction="row" 
+                            useFlexGap
+                            sx={{ flexWrap: 'wrap' }}
+                            alignItems="flex-start"
+                        >
+                            <IconCard
+                                type="Language"
+                                iconSrc="/assets/german.png"
+                                altText="German"
+                                text="German"
+                                description=""
+                            />
+                            <IconCard
+                                type="Language"
+                                iconSrc="/assets/english.png"
+                                altText="English"
+                                text="English"
+                                description=""
+                            />
+                            <IconCard
+                                type="Language"
+                                iconSrc="/assets/chinese.png"
+                                altText="Chinese"
+                                text="Chinese"
+                                description=""
+                            />
+                        </Stack>
+
+                        <Divider sx={{ padding: '8px' }}/>
+
+<Typography
+    variant="h5"
+    gutterBottom
+    sx={{ textAlign: 'left' }}
+>
+    我的爱好
+</Typography>
+<Stack 
+    spacing={{ xs: 1, sm: 2 }}
+    direction="row" 
+    useFlexGap
+    sx={{ flexWrap: 'wrap' }}
+    alignItems="flex-start"
+>
+    <IconCard
+        type="Language"
+        iconSrc="/assets/books.png"
+        altText="books"
+        text="Reading"
+        description=""
+    />
+</Stack>
                     </Paper>
                 </motion.div>
             </Container>
@@ -200,6 +407,14 @@ const styles = {
     paper: {
         padding: '2rem',
         marginBottom: '2rem',
+    },
+    icon: {
+        width: '36px',
+        height: '36px',
+    },
+    inlineContainer: {
+        display: 'flex',
+        alignItems: 'center',
     },
 };
 
