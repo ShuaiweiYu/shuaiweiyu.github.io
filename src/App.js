32c7c3f2ab45e7b5c711b919bf3cb2fe5ecde4c5
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2024-12-25 21:19:41 +0100

    add projects page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
 import MyAppBar from "./components/MyAppBar";
 import Box from "@mui/material/Box";
 
@@ -11,6 +12,9 @@ function App() {
           <Routes>
               <Route path="/" element={<Layout />}>
                   <Route index element={<HomePage />} />
+              </Route>
+              <Route path="/projects" element={<Layout />}>
+                  <Route element={<HomePage />} />
               </Route>
           </Routes>
       </Box>
