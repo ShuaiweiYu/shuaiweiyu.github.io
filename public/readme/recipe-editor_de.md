4bf8471a89d14c3442fd603dcd122f3425beef19
Shuaiwei Yu <shuaiwei.yu@siemens.com>
2025-03-27 18:47:22 +0100

    update the project specific readme

---
+++ public/readme/recipe-editor_de.md
@@ -0,0 +1,10 @@
+# React Markdown Example
+
+- Some text
+- Some other text
+
+## Subtitle
+
+### Additional info
+
+This is a [link](https://github.com/remarkjs/react-markdown)
--- src/pages/projects/RecipeEditor.js
+++ src/pages/projects/RecipeEditor.js
@@ -1,22 +1,20 @@
 import ReactMarkdown from "react-markdown";
-
-const markdownText = `
-# React Markdown Example
-
-- Some text
-- Some other text
-
-## Subtitle
-
-### Additional info
-
-This is a [link](https://github.com/remarkjs/react-markdown)
-`;
+import React, { useEffect, useState } from "react";
 
 function MarkdownComponent() {
+    const [markdownContent, setMarkdownContent] = useState("");
+
+    useEffect(() => {
+        fetch("/readme/recipe-editor_en.md")
+            .then((response) => response.text())
+            .then((text) => setMarkdownContent(text))
+            .catch((error) => console.error("Error loading markdown file:", error));
+    }, []);
+
+
     return (
-        <section>
-            <ReactMarkdown>{markdownText}</ReactMarkdown>
+        <section style={{ padding: "0 20px" }}>
+            <ReactMarkdown>{markdownContent}</ReactMarkdown>
         </section>
     );
 }
